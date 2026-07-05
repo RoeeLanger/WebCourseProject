7c748163800124a28a106a548fd26ba966955925
RoeeLanger <118664631+RoeeLanger@users.noreply.github.com>
2026-07-05 10:11:14 +0300

    Delete Part3/db.config.js
    accidentally commited db config

--- Part3/db.config.js
+++
@@ -1,7 +0,0 @@
-// Local MySQL credentials. Hosted deploys read env vars instead (see db.js).
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "1234Roeelanger1",
-  DB: "bookies"
-};
